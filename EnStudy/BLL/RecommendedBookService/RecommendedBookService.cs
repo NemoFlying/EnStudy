49cf1ccca5edfa35dc7c0701d39f8d7fb2409074
NemoFlying <895190626@qq.com>
2019-03-18 20:46:24 +0800

    添加书籍

---
+++ EnStudy/BLL/RecommendedBookService/RecommendedBookService.cs
@@ -0,0 +1,68 @@
+﻿using EnStudy.DAL;
+using EnStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnStudy.BLL
+{
+    public class RecommendedBookService: IRecommendedBookService
+    {
+        private readonly IRecommendedBookDAL _book;
+
+        public RecommendedBookService()
+        {
+            _book = new RecommendedBookDAL();
+        }
+
+        /// <summary>
+        /// 获取推荐书籍列表
+        /// </summary>
+        /// <returns></returns>
+        public ResultOutput GetRecommendedBooks()
+        {
+            var result = new ResultOutput();
+            result.Data = _book.GetModels(con => 1 == 1).ToList();
+            result.Status = true;
+            return result;
+        }
+
+        /// <summary>
+        /// 添加推荐书籍
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public ResultOutput AddRecommendedBook(RecommendedBook input)
+        {
+            var result = new ResultOutput();
+            _book.Add(input);
+
+            try
+            {
+                _book.SaveChanges();
+                result.Status = true;
+                result.Data = _book.GetModels(con => 1 == 1).ToList();
+            }
+            catch(Exception e)
+            {
+                result.Data = e;
+            }
+            return result;
+
+        }
+
+        public ResultOutput DeleteRecommendedBook(int id)
+        {
+            var result = new ResultOutput();
+            var book = _book.GetModels(con => con.Id == id).FirstOrDefault();
+            return result;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ EnStudy/Models/RecommendedBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EnStudy.Models
+{
+    /// <summary>
+    /// 推荐书籍
+    /// </summary>
+    public class RecommendedBook
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //设置自动增长
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 书籍名称
+        /// </summary>
+        public string BookName { get; set; }
+
+        /// <summary>
+        /// 书籍简介
+        /// </summary>
+        public string BookIntroduction { get; set; }
+
+        /// <summary>
+        /// 书籍图片地址
+        /// </summary>
+        public string BookImgUrl { get; set; }
+
+        /// <summary>
+        /// 书籍购买连接
+        /// </summary>
+        public string BookShopUrl { get; set; }
+
+        public DateTime MyProperty { get; set; } = DateTime.Now;
+
+    }
+}
